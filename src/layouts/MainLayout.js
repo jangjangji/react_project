599c3b95aa76fbb5acf0645d4e28ada78d469e41
jangjangji <sjjang41@naver.com>
2024-04-15 17:17:18 +0900

    스타일링 공통 헤더 푸터 구성 완료

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../outlines/Header';
 import Footer from '../outlines/Footer';
+import styled from 'styled-components';
+
+const MainBox = styled.main`
+  min-height: 650px;
+
+`;
 
 const MainLayout = () => {
   return (
     <>
       <Header />
+      <MainBox>
       <main>
         <Outlet />
       </main>
+      </MainBox>
       <Footer />
     </>
   );
